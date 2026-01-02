a5c17b51ff74d269746723c728fa4ce5b31478f5
Patrik Cesnek <pcesnek290@gmail.com>
2026-01-02 14:35:38 +0100

    Initial Commit

---
+++ Atlas/AtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasApp.swift
+//  Atlas
+//
+//  Created by Patrik Cesnek on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AtlasTests/AtlasTests.swift
@@ -0,0 +1,16 @@
+//
+//  AtlasTests.swift
+//  AtlasTests
+//
+//  Created by Patrik Cesnek on 02/01/2026.
+//
+
+import Testing
+
+struct AtlasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
